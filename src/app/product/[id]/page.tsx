0777e387b15e6d22a0854cdd7952701990140de6
Islam Abdelzaher <islam.abdelzaher@gmail.com>
2025-08-03 15:21:34 +0300

    chore: setup MongoDB connection with Mongoose

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,22 @@
+import { dbConnect } from "@/lib/dbConnect";
+
 type Props = {
   params: { id: string };
 };
 
-function ProductDetailsPage({ params }: Props) {
-  return <h1 className="text-xl">Product Details for ID: {params.id}</h1>;
+async function ProductDetailsPage({ params }: Props) {
+  try {
+    await dbConnect();
+    console.log("✅ MongoDB Connected from UI page");
+  } catch (error) {
+    console.error("❌ Connection Error:", error);
+  }
+
+  return (
+    <div>
+      <h1>Product Details Page</h1>
+    </div>
+  );
 }
 
 export default ProductDetailsPage;
